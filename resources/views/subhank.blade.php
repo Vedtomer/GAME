46656c125b119d1a0941314fbe6cc688d2bd0f81
Ved <vedtomer5592@gmail.com>
2024-01-22 00:34:05 +0530

    fixed

--- resources/views/subhank.blade.php
+++ resources/views/subhank.blade.php
@@ -57,7 +57,8 @@
             <table class="table responsive" cellspacing="0" id="grda" style="width:100%;border-collapse:collapse;">
                 <thead>
                     <tr>
-                        <th style="width: 10% ;text-align: center;" scope="col">                    <input type="date" id="dateFilter" onchange="fetchData()" style="flex: 0 0 auto;  "></th>
+                        <th style="width: 10% ;text-align: center;" scope="col">                   
+                             <input type="date" id="dateFilter" onchange="fetchData()" style="flex: 0 0 auto;  "></th>
                         <th style="width: 10%;text-align: center;" scope="col"> <img src="{{ asset('logo12_files/601.png') }}" style="height:50px;"></th>
                         <th style="width: 10%;text-align: center;" scope="col"> <img src="{{ asset('logo12_files/70gh.png') }}" style="height:50px;  flex: 0 0 auto; margin-left: 10px;"></th>
                     </tr>
@@ -81,7 +82,7 @@
                         @else
                         <tr> 
                            
-                            <td style="text-align: center;">No Data Found</td>
+                            <td style="text-align: center;" colspan="3">No Data Found</td>
                         </tr>
                     @endif
             </tbody>
@@ -101,9 +102,7 @@
 </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                fetchData(new Date().toISOString().split('T')[0]);
-            });
+          
         
             function fetchData(date) {
                 var selectedDate = date || document.getElementById('dateFilter').value;
