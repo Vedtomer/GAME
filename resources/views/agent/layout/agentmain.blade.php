adff1789de79493c6e8fb08e69a13da20d4caea0
Ved <vedtomer5592@gmail.com>
2024-01-21 22:47:07 +0530

    fixed

--- resources/views/agent/layout/agentmain.blade.php
+++ resources/views/agent/layout/agentmain.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
 
     <title> @yield('title', 'Agent Dashboard')</title>
     <meta name="viewport"
