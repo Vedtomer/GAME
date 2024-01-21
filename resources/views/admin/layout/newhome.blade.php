8fe37890c49496da167cb76bb612bf4808345aed
Ved <vedtomer5592@gmail.com>
2024-01-21 21:50:26 +0530

    fixed

--- resources/views/admin/layout/newhome.blade.php
+++ resources/views/admin/layout/newhome.blade.php
@@ -3,11 +3,11 @@
 
 <head>
     <title>Golden Lucky Draw</title>
-    <meta charset="utf-8">
+    {{-- <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
     <link rel="stylesheet" href="{{ asset('homes.css') }}">
     <script src="{{ asset('homejs.js') }}"></script>
     <script src="{{ asset('home3.js') }}"></script>
