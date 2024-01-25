394fc2210d7f93d4519d5d344419d52bbb2d2cd6
ved <vedtomer5592@gamil.com>
2024-01-25 17:16:05 +0530

    ok

--- resources/views/agent/dashboard.blade.php
+++ resources/views/agent/dashboard.blade.php
@@ -649,102 +649,103 @@ echo "<th class='result'><span style='color: red;'>" . ($data[$lastSetIndex]['nu
     
     
     
-<script>
- (function($) {
-  var CheckboxDropdown = function(el) {
-    var _this = this;
-    this.isOpen = false;
-    this.areAllChecked = false;
-    this.$el = $(el);
-    this.$label = this.$el.find('.dropdown-label');
-    this.$checkAll = this.$el.find('[data-toggle="check-all"]').first();
-    this.$inputs = this.$el.find('[type="checkbox"]');
+    <script>
+      (function($) {
+        var CheckboxDropdown = function(el) {
+          var _this = this;
+          this.isOpen = false;
+          this.areAllChecked = false;
+          this.$el = $(el);
+          this.$label = this.$el.find('.dropdown-label');
+          this.$checkAll = this.$el.find('[data-toggle="check-all"]').first();
+          this.$inputs = this.$el.find('[type="checkbox"]');
+          
+          this.onCheckBox();
+          
+          this.$label.on('click', function(e) {
+            e.preventDefault();
+            _this.toggleOpen();
+          });
+          
+          this.$checkAll.on('click', function(e) {
+            e.preventDefault();
+            _this.onCheckAll();
+          });
+          
+          this.$inputs.on('change', function(e) {
+            _this.onCheckBox();
+          });
+        };
+        
+        CheckboxDropdown.prototype.onCheckBox = function() {
+          this.updateStatus();
+        };
+        
+        CheckboxDropdown.prototype.updateStatus = function() {
+          var checked = this.$el.find(':checked');
+          console.log(checked.length, this.areAllChecked);
     
-    this.onCheckBox();
+          this.areAllChecked = false;
+          this.$checkAll.html('Select All');
+          
+          if (checked.length <= 0) {
+            this.$label.html('Select');
+          } else if (checked.length === 1) {
+            this.$label.html(checked.parent('label').text());
+          } else if (checked.length === this.$inputs.length) {
+            console.log("All selected");
+            this.$label.html('All Selected');
+            this.areAllChecked = true;
+            this.$checkAll.html('Uncheck All');
+          } else {
+            this.$label.html(checked.length + ' Selected');
+          }
+          updateQty();
+        };
     
-    this.$label.on('click', function(e) {
-      e.preventDefault();
-      _this.toggleOpen();
-    });
-    
-    this.$checkAll.on('click', function(e) {
-      e.preventDefault();
-      _this.onCheckAll();
-    });
-    
-    this.$inputs.on('change', function(e) {
-      _this.onCheckBox();
-    });
-  };
-  
-  CheckboxDropdown.prototype.onCheckBox = function() {
-    this.updateStatus();
-  };
-  
-  CheckboxDropdown.prototype.updateStatus = function() {
-    var checked = this.$el.find(':checked');
-    // console.log(checked.length);
-    this.areAllChecked = false;
+        CheckboxDropdown.prototype.onCheckAll = function(checkAll) {
+  console.log(this.areAllChecked);
+
+  // Toggle the state of areAllChecked
+  this.areAllChecked = !this.areAllChecked;
+
+  if (this.areAllChecked || checkAll) {
+    this.$checkAll.html('Uncheck All');
+    this.$inputs.prop('checked', true);
+  } else {
     this.$checkAll.html('Select All');
-    
-    if(checked.length <= 0) {
-      this.$label.html('Select');
-    }
-    else if(checked.length === 1) {
-      this.$label.html(checked.parent('label').text());
-    }
-    else if(checked.length === this.$inputs.length) {
-      this.$label.html('All Selected');
-      this.areAllChecked = true;
-      this.$checkAll.html('UnSelectAll');
-    }
-    else {
-      this.$label.html(checked.length + ' Selected');
-    }
-    updateQty();
-  };
-
-  CheckboxDropdown.prototype.onCheckAll = function(checkAll) {
-    if(!this.areAllChecked || checkAll) {
-      this.areAllChecked = true;
-      this.$checkAll.html('Uncheck All');
-      this.$inputs.prop('checked', true);
-    }
-    else {
-      this.areAllChecked = false;
-      this.$checkAll.html('Check All');
-      this.$inputs.prop('checked', false);
-    }
-    
-    this.updateStatus();
-  };
-  
-  CheckboxDropdown.prototype.toggleOpen = function(forceOpen) {
-    var _this = this;
-    
-    if(!this.isOpen || forceOpen) {
-       this.isOpen = true;
-       this.$el.addClass('on');
-      $(document).on('click', function(e) {
-        if(!$(e.target).closest('[data-control]').length) {
-         _this.toggleOpen();
-        }
-      });
-    }
-    else {
-      this.isOpen = false;
-      this.$el.removeClass('on');
-      $(document).off('click');
-    }
-  };
-  
-  var checkboxesDropdowns = document.querySelectorAll('[data-control="checkbox-dropdown"]');
-  for(var i = 0, length = checkboxesDropdowns.length; i < length; i++) {
-    new CheckboxDropdown(checkboxesDropdowns[i]);
+    this.$inputs.prop('checked', false);
   }
-})(jQuery);
 
-</script>
+  // this.updateStatus();
+};
+
+        
+        CheckboxDropdown.prototype.toggleOpen = function(forceOpen) {
+          var _this = this;
+          
+          if (!this.isOpen || forceOpen) {
+             this.isOpen = true;
+             this.$el.addClass('on');
+            $(document).on('click', function(e) {
+              if (!$(e.target).closest('[data-control]').length) {
+               _this.toggleOpen();
+              }
+            });
+          } else {
+            this.isOpen = false;
+            this.$el.removeClass('on');
+            $(document).off('click');
+          }
+        };
+        
+        var checkboxesDropdowns = document.querySelectorAll('[data-control="checkbox-dropdown"]');
+        for (var i = 0, length = checkboxesDropdowns.length; i < length; i++) {
+          new CheckboxDropdown(checkboxesDropdowns[i]);
+        }
+      })(jQuery);
+    </script>
+    
 
     <script>
         function validateForm() {
