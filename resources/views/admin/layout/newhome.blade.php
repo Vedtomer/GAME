18fb1b25d22ccdca41d5146a007a9cc8858b842d
Ved <vedtomer5592@gmail.com>
2024-02-20 23:24:12 +0530

    fixed

--- resources/views/admin/layout/newhome.blade.php
+++ resources/views/admin/layout/newhome.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="{{ asset('homes.css') }}">
     <script src="{{ asset('homejs.js') }}"></script>
