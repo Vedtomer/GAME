a72c949bfd0e7d85f14683dc27dc6bb9329d1155
ved <vedtomer5592@gamil.com>
2024-02-21 13:34:06 +0530

    ok

--- resources/views/subhank.blade.php
+++ resources/views/subhank.blade.php
@@ -44,9 +44,10 @@
   </head>
   <body>
 
-    <div class="btns mb-2" style="margin-right: auto;">
+    <div class="btns mb-2" style="margin-right: auto; ">
                    
-        <a href="{{ route('dashboard') }}" class="btn btn-info">Back</a>
+        {{-- <a href="{{ route('dashboard') }}" >Back</a> --}}
+        <a onclick="history.back()" class="btn btn-info" style="height: 50px; margin:8px; font-size:25px;">Go Back</a>
     </div>
 <form method="post" action="{{ route('subhank') }}" id="form1">
     @csrf
