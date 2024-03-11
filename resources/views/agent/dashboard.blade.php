bfc90694ce4f50c6a2acfd30d040ee04ce72b347
ved <vedtomer5592@gamil.com>
2024-03-11 15:21:41 +0530

    ok

--- resources/views/agent/dashboard.blade.php
+++ resources/views/agent/dashboard.blade.php
@@ -8,10 +8,11 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
     <title> @yield('title', 'Agent Dashboard')</title>
+    <link rel="stylesheet" href="{{ asset('main.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 
     {{-- <link rel="stylesheet" href="{{ asset('main.css') }}"> --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
 
 
 </head>
